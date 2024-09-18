75480fa02e01978f0ae5965b7e21d129b35054fd
Jonas Herzfeld <jonas.herzfeld@gmail.com>
2024-09-18 19:52:08 +0200

    Fix vercel deployment of flask app (#11)

--- packages/server/api/run.py
+++ packages/server/api/run.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
-from config import app, db
-from models import Contact
+from .config import app, db
+from .models import Contact
 
 @app.route("/")
 @app.route("/contacts")
