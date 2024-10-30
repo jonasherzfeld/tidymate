0cf16f85fcb3249d76f17cedfefdc17768ce5211
Jonas Herzfeld <jonas.herzfeld@gmail.com>
2024-10-30 16:33:57 +0100

    Fix redirect loop (#31)

--- packages/web/src/routes/+page.server.js
+++ packages/web/src/routes/+page.server.js
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ locals }) {
     // redirect user if logged in
-    if (locals.user) {
+    if (locals.user && locals.house) {
         redirect(302, '/home');
     }
 }
