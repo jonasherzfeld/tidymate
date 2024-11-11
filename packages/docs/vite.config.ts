efded3326d0005a1e80bf1a9b9fc615eabd66be7
Jonas Herzfeld <jonas.herzfeld@gmail.com>
2024-11-11 12:12:41 +0100

    Cleanup Docs with construction notice

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,63 +1,67 @@
-import { defineConfig } from 'vite'
-import { sveltepress } from '@sveltepress/vite'
-import { defaultTheme } from '@sveltepress/theme-default'
+import { defineConfig } from "vite";
+import { sveltepress } from "@sveltepress/vite";
+import { defaultTheme } from "@sveltepress/theme-default";
 
 const config = defineConfig({
   plugins: [
     sveltepress({
       theme: defaultTheme({
-        navbar : [
+        navbar: [
+          // {
+          //   title : 'Foo page',
+          //   to : '/foo/'
+          // },
+          // {
+          //   title : 'With dropdown',
+          //   items : [
+          //     {
+          //       title : 'Bar page',
+          //       to : '/foo/bar/'
+          //     },
+          //     {
+          //       title : 'External Github page',
+          //       to : 'https://github.com/',
+          //       external : true
+          //     }
+          //   ]
+          // }
           {
-            title : 'Foo page',
-            to : '/foo/'
+            title: "App",
+            to: "https://tidymate.vercel.app/",
+            external: true,
           },
-          {
-            title : 'With dropdown',
-            items : [
-              {
-                title : 'Bar page',
-                to : '/foo/bar/'
-              },
-              {
-                title : 'External Github page',
-                to : 'https://github.com/',
-                external : true
-              }
-            ]
-          }
         ],
-        sidebar : {
-          '/': [
-            {
-              title : 'Bar',
-              to : '/foo/',
-            },
-            {
-              title : 'Zoo',
-              collapsible : true,
-              items : [
-                {
-                  title : 'Sub item',
-                  to : '/foo/bar'
-                }
-              ]
-            },
-            {
-              title : 'External github page',
-              to : 'https://github.com'
-            }
-          ]
+        sidebar: {
+          // '/': [
+          //   {
+          //     title : 'Bar',
+          //     to : '/foo/',
+          //   },
+          //   {
+          //     title : 'Zoo',
+          //     collapsible : true,
+          //     items : [
+          //       {
+          //         title : 'Sub item',
+          //         to : '/foo/bar'
+          //       }
+          //     ]
+          //   },
+          //   {
+          //     title : 'External github page',
+          //     to : 'https://github.com'
+          //   }
+          // ]
         },
-        github: 'https://github.com/Blackman99/sveltepress',
-        logo: '/sveltepress.svg',
+        github: "https://github.com/jonasherzfeld/tidymate",
+        logo: "/tidymate_logo.png",
       }),
       siteConfig: {
-        title: 'Sveltepress',
-        description: 'A content centered site build tool',
+        title: "Tidymate",
+        description: "A lean organizer app for you and your mates!",
       },
     }),
   ],
-})
+});
 
-export default config
-
+export default config;
