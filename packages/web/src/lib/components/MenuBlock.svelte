d172e84de39c50c91aa21e1fc9d30999349fece7
Jonas Herzfeld <jonas.herzfeld@gmail.com>
2024-11-28 15:50:07 +0100

    Move Menu to separate component

--- packages/web/src/lib/components/MenuBlock.svelte
+++ packages/web/src/lib/components/MenuBlock.svelte
@@ -8,22 +8,33 @@
     let {
         restricted,
         position,
-        handleClick
-    }: { restricted: RestrictionType[]; position: RouteLinkPosition; handleClick?: () => void } =
-        $props();
+        handleClick,
+        buttonView
+    }: {
+        restricted: RestrictionType[];
+        position: RouteLinkPosition;
+        handleClick?: () => void;
+        buttonView?: boolean;
+    } = $props();
 </script>
 
 {#each ROUTE_MAPPING as item}
     {#if item.position.includes(position)}
         {#if restricted.includes(item.restricted) || item.restricted === ('none' as RestrictionType)}
             <li class="text-base">
-                {#if handleClick}
-                    <a href={item.url} target={item.external ? '_blank' : ''} onclick={handleClick}
-                        ><item.icon />{item.title}</a
-                    >
+                {#if !buttonView}
+                    {#if handleClick}
+                        <a href={item.url} target={item.target} onclick={handleClick}
+                            ><item.icon />{item.title}</a
+                        >
+                    {:else}
+                        <a href={item.url} target={item.target}><item.icon />{item.title}</a>
+                    {/if}
                 {:else}
-                    <a href={item.url} target={item.external ? '_blank' : ''}
-                        ><item.icon />{item.title}</a
+                    <a href={item.url} target={item.target} class="p-0"
+                    ><button class="btn btn-md btn-square btn-ghost text-xs p-0 gap-0"
+                        ><item.icon class="w-6 h-6"/>{item.title}</button
+                    ></a
                     >
                 {/if}
             </li>
