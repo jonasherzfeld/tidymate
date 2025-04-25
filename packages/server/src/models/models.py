74610ed875e5b884bc7464b9d3d34faad547d0aa
Jonas Herzfeld <jonas.herzfeld@gmail.com>
2025-04-25 22:16:07 +0200

    POC for local SQLITE database

--- packages/server/src/models/models.py
+++ packages/server/src/models/models.py
@@ -1,4 +1,8 @@
 from enum import Enum
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+
+from config import app, db, bcrypt
 
 
 class ChoreSeverity(Enum):
@@ -6,6 +10,7 @@ class ChoreSeverity(Enum):
     MEDIUM = 1
     HIGH = 2
 
+    @staticmethod
     def from_int(severity: int) -> 'ChoreSeverity':
         try:
             output = ChoreSeverity(severity)
@@ -14,207 +19,92 @@ class ChoreSeverity(Enum):
         return output
 
 
-class Todo():
-    id: str = ""
-    data: str = ""
-    assignee: str = ""
-    done = False
-    tags: list = []
-    created_on: str = ""
-    deadline: str = ""
+class Users(db.Model, SerializerMixin):
+    id = db.Column("id", db.Integer, primary_key=True)
+    email = db.Column("email", db.String(30), nullable=False, unique=True)
+    _password_hash = db.Column("password_hash", db.String(100), nullable=False)
+    first_name = db.Column("first_name", db.String(100))
+    last_name = db.Column("last_name", db.String(100))
+    joined_on = db.Column("joined_on", db.String(100))
+    is_admin = db.Column("is_admin", db.Boolean, default=False)
+    house_id = db.Column("house_id", db.Integer, db.ForeignKey('house.id'))
+    house = db.relationship('House', back_populates='members')
 
-    def __init__(
-            self,
-            id: str = None,
-            data: str = None,
-            assignee: str = None,
-            done=False,
-            tags: list = None,
-            created_on: str = None,
-            deadline: str = None):
-        self.id = id
-        self.data = data
-        self.assignee = assignee
-        self.done = done
-        self.tags = tags
-        self.created_on = created_on
-        self.deadline = deadline
-
-    def from_json(self, data: dict):
-        self.id = data.get("id", "")
-        self.data = data.get("data", "")
-        self.assignee = data.get("assignee", "")
-        self.done = data.get("done", False)
-        self.tags = data.get("tags", [])
-        self.created_on = data.get("created_on", "")
-        self.deadline = data.get("deadline", "")
-        return self
-
-    def to_json(self) -> dict:
-        return {
-            "id": self.id,
-            "data": self.data,
-            "assignee": self.assignee,
-            "done": self.done,
-            "tags": self.tags,
-            "created_on": self.created_on,
-            "deadline": self.deadline
-        }
-
-
-class Chore(Todo):
-    frequency: int = 0  # Days
-    last_done: str = ""
-    room: str = ""
-    severity: int = ChoreSeverity.LOW
-
-    def __init__(
-            self,
-            id: str = None,
-            data: str = None,
-            assignee: str = None,
-            done=False,
-            tags: list = None,
-            created_on: str = None,
-            deadline: str = None,
-            frequency: int = None,
-            last_done: str = None,
-            room: str = None,
-            severity: ChoreSeverity = None):
-        super().__init__(id, data, assignee, done, tags, created_on, deadline)
-        self.frequency = frequency
-        self.last_done = last_done
-        self.room = room
-        self.severity = severity
-
-    def from_json(self, data: dict):
-        self.id = data.get("id", "")
-        self.data = data.get("data", "")
-        self.assignee = data.get("assignee", "")
-        self.tags = data.get("tags", [])
-        self.created_on = data.get("created_on", "")
-        self.deadline = data.get("deadline", "")
-        self.frequency = data.get("frequency", 0)
-        self.last_done = data.get("last_done", "")
-        self.room = data.get("room", "")
-        self.severity = ChoreSeverity.from_int(data.get("severity", 0))
-        return self
-
-    def to_json(self) -> dict:
-        return {
-            "id": self.id,
-            "data": self.data,
-            "assignee": self.assignee,
-            "tags": self.tags,
-            "created_on": self.created_on,
-            "deadline": self.deadline,
-            "frequency": self.frequency,
-            "last_done": self.last_done,
-            "room": self.room,
-            "severity": self.severity.value if self.severity else 0
-        }
-
-
-class User():
-    id: str = ""
-    email: str = ""
-    first_name: str = ""
-    last_name: str = ""
-    joined_on: str = ""
-    house_id: str = ""
-    is_admin: bool = False
-
-    def __init__(
-            self,
-            id: str = None,
-            email: str = None,
-            first_name: str = None,
-            last_name: str = None,
-            joined_on: str = None,
-            house_id: str = None,
-            thumbnail: str = None,
-            is_admin: bool = False):
-        self.id = id
+    def __init__(self, email, password, first_name, last_name,
+                 joined_on, is_admin, house):
         self.email = email
+        self.password_hash = password
         self.first_name = first_name
         self.last_name = last_name
         self.joined_on = joined_on
-        self.house_id = house_id
-        self.thumbnail = thumbnail
         self.is_admin = is_admin
+        self.house_id = house.id if house else None
+        self.house = house
 
-    def from_json(self, data: dict):
-        self.id = data.get("id", "")
-        self.email = data.get("email", "")
-        self.first_name = data.get("first_name", "")
-        self.last_name = data.get("last_name", "")
-        self.joined_on = data.get("joined_on", "")
-        self.house_id = data.get("house_id", "")
-        self.thumbnail = data.get("thumbnail", "")
-        self.is_admin = data.get("is_admin", False)
-        return self
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
 
-    def to_json(self) -> dict:
-        return {
-            "id": self.id,
-            "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "joined_on": self.joined_on,
-            "house_id": self.house_id,
-            "thumbnail": self.thumbnail,
-            "is_admin": self.is_admin
-        }
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    # this prevents that Exception being raised everytime we try to call the
+    # .to_dict() method in a request that returns information from users
+    serialize_rules = ('-_password_hash', '-house.members',
+                       '-house.chores', '-house.todos')
 
 
-class House():
-    id: str = ""
-    name: str = ""
-    city: str = ""
-    country: str = ""
-    created_on: str = ""
-    join_id: str = ""
-    members: list = []
-    rooms: list = []
+class House(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    join_id = db.Column(db.String(100))
+    created_on = db.Column(db.String(100))
+    rooms = db.Column(db.String(100))
+    members = db.relationship('Users', back_populates='house')
+    chores = db.relationship('Chore', back_populates='house')
+    todos = db.relationship('Todo', back_populates='house')
 
-    def __init__(
-            self,
-            id: str = None,
-            name: str = None,
-            city: str = None,
-            country: str = None,
-            created_on: str = None,
-            join_id: str = None,
-            members: list = None,
-            rooms: list = None):
-        self.id = id
-        self.name = name
-        self.city = city
-        self.country = country
-        self.created_on = created_on
-        self.join_id = join_id
-        self.members = members
-        self.rooms = rooms
+    serialize_rules = ('-members.house', '-chores.house', '-todos.house')
 
-    def from_json(self, data: dict):
-        self.id = data.get("id", "")
-        self.name = data.get("name", "")
-        self.city = data.get("city", "")
-        self.country = data.get("country", "")
-        self.created_on = data.get("created_on", "")
-        self.join_id = data.get("join_id", "")
-        self.members = data.get("members", [])
-        self.rooms = data.get("rooms", [])
-        return self
 
-    def to_json(self) -> dict:
-        return {
-            "id": self.id,
-            "name": self.name,
-            "city": self.city,
-            "country": self.country,
-            "created_on": self.created_on,
-            "members": self.members,
-            "join_id": self.join_id,
-            "rooms": self.rooms
-        }
+class Todo(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(255))
+    assignee = db.Column(db.String(100))
+    done = db.Column(db.Boolean, default=False)
+    created_on = db.Column(db.String(100))
+    deadline = db.Column(db.String(100))
+    house_id = db.Column("house_id", db.Integer, db.ForeignKey('house.id'))
+    house = db.relationship('House', back_populates='todos')
+
+    serialize_rules = ('-house.todos', '-house.members', '-house.chores')
+
+
+class Chore(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(255))
+    assignee = db.Column(db.String(100))
+    done = db.Column(db.Boolean, default=False)
+    created_on = db.Column(db.String(100))
+    deadline = db.Column(db.String(100))
+    frequency = db.Column(db.Integer)
+    last_done = db.Column(db.String(100))
+    room = db.Column(db.String(100))
+    severity = db.Column(db.Enum(ChoreSeverity))
+    house_id = db.Column("house_id", db.Integer, db.ForeignKey('house.id'))
+    house = db.relationship('House', back_populates='chores')
+
+    serialize_rules = ('-house.chores', '-house.members', '-house.todos')
+
+
+with app.app_context():
+    db.create_all()
